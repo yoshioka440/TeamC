c987d7a8e8d75f0781dbb84c5ee752135e0b22ff
teruyakusumoto <teruyakusumoto@gmail.com>
2014-05-10 23:26:23 +0900

    add HumanPlaceRequestController

---
+++ TeamC/HumanPlaceRequestController.h
@@ -0,0 +1,16 @@
+//
+//  HumanPlaceRequestController.h
+//  TeamC
+//
+//  Created by teruyakusumoto on 2014/05/10.
+//  Copyright (c) 2014年 Unko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HumanPlaceRequestController : NSObject<NSURLConnectionDataDelegate>{
+    NSMutableData *tempdata;
+    NSMutableArray *adressdata;
+}
+-(void)RequestStart;
+@end
--- TeamC/ViewController.m
+++ TeamC/ViewController.m
@@ -150,7 +150,6 @@
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
